5a45b4629a70237c8cbfeb60d7048c1ab453d863
victorstephen26 <victorstephenikechukwu24@gmail.com>
2023-04-05 10:42:24 -0700

    python

--- 0x08-python-more_classes/1-main.py
+++ 0x08-python-more_classes/1-main.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
 Rectangle = __import__('1-rectangle').Rectangle
 
-rectangle = Rectangle(2, 4)
-print(rectangle.__dict__)
+my_rect = Rectangle(2, 4)
+print(my_rect.__dict__)
 
-rectangle.width = 10
-rectangle.height = 3
-print(rectangle__dict__)
+my_rect.width = 10
+my_rect.height = 3
+print(my_rect.__dict__)
