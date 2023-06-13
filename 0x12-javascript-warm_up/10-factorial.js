8eb30470a09916b95028184694147ccdfdf04c49
victorstephen26 <victorstephenikechukwu24@gmail.com>
2023-06-13 13:00:10 -0700

    JavaScript

---
+++ 0x12-javascript-warm_up/10-factorial.js
@@ -0,0 +1,13 @@
+#!/usr/bin/node
+
+function factorial (n) {
+  if (isNaN(n) || n === 0) {
+    return 1;
+  } else if (n < 0) {
+    return -1;
+  } else {
+    return n * factorial(n - 1);
+  }
+}
+const input = number(process.argv[2]);
+console.log(`factorial: ${factorial(input)}`);
